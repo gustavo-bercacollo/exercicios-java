3030f5e32d44240db2f5f1ac6a3d50e829d496c1
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-04-11 23:54:40 -0300

    exercise 05

--- dataehora/Ex04.java
+++ dataehora/Ex04.java
@@ -1,5 +1,18 @@
 package dataehora;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
 public class Ex04 {
+
+	public static void main(String[] args) {
+		LocalDate hojeMais30Dias = LocalDate.now().plusDays(30);
+		
+		
+		String dataFormatada = hojeMais30Dias.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		
+		System.err.println("Daqui a 30 dias será: " + dataFormatada);
+	}
 
 }
--- dataehora/ex05.java
+++ dataehora/ex05.java
@@ -1,5 +1,18 @@
 package dataehora;
 
-public class ex05 {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		LocalDate hojeMenos30Dias = LocalDate.now().minusWeeks(2);
+		
+		
+		String dataFormatada = hojeMenos30Dias.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		
+		System.err.println(" Duas semanas atrás: " + dataFormatada);
+	}
 
 }
