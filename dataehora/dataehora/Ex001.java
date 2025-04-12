23698bcb5748d2026f26ea77edf1c8128b4dbf2c
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-04-11 22:09:52 -0300

    exercise 01

---
+++ dataehora/dataehora/Ex001.java
@@ -0,0 +1,13 @@
+package dataehora;
+
+import java.time.LocalDateTime;
+
+public class Ex001 {
+
+	public static void main(String[] args) {
+		LocalDateTime data = LocalDateTime.now();
+		
+		System.out.println(data);
+	}
+
+}
